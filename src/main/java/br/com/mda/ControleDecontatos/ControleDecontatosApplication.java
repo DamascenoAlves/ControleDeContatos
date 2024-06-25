d6b40f6ad8b93cd1729e0bcd85deaf2a9bf626cd
Matheus Damasceno Alves <matheusd.alves@hotmail.com>
2024-06-25 18:35:41 -0300

    first commit

---
+++ src/main/java/br/com/mda/ControleDecontatos/ControleDecontatosApplication.java
@@ -0,0 +1,13 @@
+package br.com.mda.ControleDecontatos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleDecontatosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleDecontatosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/mda/ControleDecontatos/ControleDecontatosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mda.ControleDecontatos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleDecontatosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
